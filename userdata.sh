f66a99d832fa7c6def46f53439007af817496998
hasantoygun23 <h.toygun1990@hotmail.com>
2025-01-05 19:38:10 +0300

    second

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,7 @@ dnf install httpd -y
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
-FOLDER="https://raw.githubusercontent.com/awsdevopsteam/101-kittens-carousel-staticwebsite-ec2/main/static-web"
+FOLDER="https://raw.githubusercontent.com/hasantoygun23/aws-kitten-101-ec2/refs/heads/main/static-web"
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
